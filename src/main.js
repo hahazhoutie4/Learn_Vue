29bc0c3780039fcaf912365017697220e468cb01
hahazhoutie4 <1669005765@qq.com>
2025-02-18 10:35:09 +0800

    add empView

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+// 引入element ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入router路由
+import router from './router'
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
